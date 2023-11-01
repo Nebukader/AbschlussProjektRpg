0ac18e754955fafc9cdde29ca77f87bea1dfb902
Dennis <91901640+Nebukader@users.noreply.github.com>
2023-11-01 15:35:39 +0100

    liste mit kleinen gegnern angelegt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 //Heros
 
-val JackONeil:Soldier = Soldier("Jack O Neil",150,)
+val JackONeil:Soldier = Soldier("Jack O Neil", 150,)
 val samanthaCarter:Scientist = Scientist("Samantha Carter",125)
 val tealC:Jaffa = Jaffa("Teal C",200)
 val daniel:Hero = Hero("Daniel Jackson",125)
@@ -9,8 +9,18 @@ val cameron:Hero = Hero("Cameron Mitchell", 175)
 val jonas:Hero = Hero("Jonnas Quinn",125)
 
 //Villains
-val JaffafootSoldier1:FootSoldier = FootSoldier("Jaffa Jotavod",80)
-val JaffafootSoldier2:FootSoldier = FootSoldier("Jaffa Ak'noan",75)
+
+val footSoldiers:MutableList<FootSoldier> = mutableListOf(
+    FootSoldier("Jaffa Jotavod",80),
+    FootSoldier("Jaffa Ak'noan",75),
+    FootSoldier("Jaffa Ak'noan",65),
+    FootSoldier("Jaffa Ak'noan",70))
+
+
+
+
+
+//Boss
 val BossBaal:Boss = Boss("Ba'al",500)
 
 fun main() {
