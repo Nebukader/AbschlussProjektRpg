5219715b5ca74e7de504f483123c88ad4873cf61
Dennis <91901640+Nebukader@users.noreply.github.com>
2023-11-01 15:34:58 +0100

    Funktion zur Ausgabe einer Liste von Gegnern mit ( name und hp ) erstellt

--- src/main/kotlin/Enemy.kt
+++ src/main/kotlin/Enemy.kt
@@ -10,53 +10,53 @@ class Zetnitika : EnemyAction ("Zat'ni katel",25)
 
 // Die möglichen Aktionen, die der Feind ausführen kann, werden in einer Liste gespeichert.
 // Diese Liste enthält Aktionen wie "Schlag" und "Stabwaffe" als vordefinierte Instanzen.
-open class Enemy(var name: String,var healthPoints: Int){
-    private val actions = listOf(StabwaffenSchuss(),TacTac(),Cannon(),Schlag(),HandGerät(),Zetnitika())
+open class Enemy(var name: String, var healthPoints: Int) {
+    private val actions = listOf(StabwaffenSchuss(), TacTac(), Cannon(), Schlag(), HandGerät(), Zetnitika())
     private val enemyName = name
 
-    fun randomAction(): Int{
+
+    fun randomAction(): Int {
         val randomAttacks = actions.random()
         val actionName = randomAttacks.name
-        println("Der Feind greift mit $actionName an ")
+        println("Der Feind greift mit $actionName an und verursacht ${randomAttacks.damage} Schadenspunkte")
         return randomAttacks.damage
     }
 
-    open fun attackInfo(){
-        println("${name} holt zum Anrgriff aus")
-        println("${randomAction()}")
-    }
-    open fun takeDamage(damage:Int){
-        healthPoints -=damage
-        if (healthPoints <= 0){
+
+    open fun takeDamage(damage: Int) {
+        healthPoints -= damage
+        if (healthPoints <= 0) {
             println("Der Feind ist tot")
 
-    }else
-        println("Der $name hat noch $healthPoints Lebenspunkte")
+        } else
+            println("Der $name hat noch $healthPoints Lebenspunkte")
+    }
 }
 
+open class FootSoldier(name:String, healthPoints: Int) : Enemy(name,healthPoints) {
+}
 
-class FootSoldier(name:String,healthPoints: Int) : Enemy(name,healthPoints){
-    override fun attackInfo() {
-        super.attackInfo()
+fun printInfoList(footSoldiers:MutableList<FootSoldier>) {
+    for (footSoldier in footSoldiers) {
+        println("${footSoldier.name}: Health Points = ${footSoldier.healthPoints}")
     }
 }
 
 open class Boss(name: String, healthPoints: Int) : Enemy(name, healthPoints){
-    override fun attackInfo() {
-        super.attackInfo()
-    }
+
 }
 
 class Minion(name: String, healthPoints: Int) : Boss(name, healthPoints){
-    override fun attackInfo() {
-        super.attackInfo()
-    }
+
+
 }
 
 
-val jaffa: Minion = Minion("Jaffa foot Soldier", 150)
+
+
+/*val jaffa: Minion = Minion("Jaffa foot Soldier", 150)
 
 val apophis: Enemy = Enemy("Apohis", 200)
 val baal: Enemy = Enemy("Baal", 225)
 val ra: Enemy = Enemy("Ra", 300)
-val heruur: Enemy = Enemy("Heru´ur", 220)
+val heruur: Enemy = Enemy("Heru´ur", 220)*/
