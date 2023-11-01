3e7f0112cb73d2129e98256aba3fedd705269359
Dennis <91901640+Nebukader@users.noreply.github.com>
2023-11-01 11:29:46 +0100

    Villians angelgt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+//Heros
+
 val JackONeil:Soldier = Soldier("Jack O Neil",150,)
 val samanthaCarter:Scientist = Scientist("Samantha Carter",125)
 val tealC:Jaffa = Jaffa("Teal C",200)
@@ -5,6 +7,12 @@ val daniel:Hero = Hero("Daniel Jackson",125)
 val bratak:Hero = Hero("Bratak",250)
 val cameron:Hero = Hero("Cameron Mitchell", 175)
 val jonas:Hero = Hero("Jonnas Quinn",125)
+
+//Villains
+val JaffafootSoldier1:FootSoldier = FootSoldier("Jaffa Jotavod",80)
+val JaffafootSoldier2:FootSoldier = FootSoldier("Jaffa Ak'noan",75)
+val BossBaal:Boss = Boss("Ba'al",500)
+
 fun main() {
 
 }
