6b14a4af96cacf7afa866649b1b410af4544521d
Dennis <91901640+Nebukader@users.noreply.github.com>
2023-11-07 10:02:16 +0100

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,7 +27,7 @@ val Bandages:MutableList<Heal> = mutableListOf(
 )
 
 val footSoldierActions = listOf(StabwaffenSchuss(), TacTac(), Cannon(), Schlag(), HandGerät(), Zetnitika())
-val bossActions = listOf(StabwaffenSchuss(),TacTac(), Schlag(), HandGerät(),AlkeshBomber(),Todesgleiter())
+val bossActions = listOf(StabwaffenSchuss(),TacTac(), Schlag(), HandGerät(),AlkeshBomber(),Todesgleiter(),Debuff(heroes.random(),"Naniten",10))
 val minionActions = listOf(PlasmaRepeater(),Schlag())
 
 
