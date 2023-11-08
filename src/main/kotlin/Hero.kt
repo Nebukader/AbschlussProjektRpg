b6f4d2e7757094b71726276804d0be685cfa8636
dennis <nebukader@gmail.com>
2023-11-08 16:18:34 +0100

    Kommentare und kleinen Damage Bonus für klasse Soldier und Jaffa eingefügt

--- src/main/kotlin/Hero.kt
+++ src/main/kotlin/Hero.kt
@@ -1,67 +1,64 @@
 open class Hero(val name: String, var healthPoints: Int, var debuff: Boolean = false) {
+
+    // **Methode um eine Aktion mit einer Waffe auszuführen**
     open fun action(weaponName: String, damage: Int): Int {
+        // Gibt den Schaden zurück, der durch die Aktion verursacht wird
+        println("Name:$name")
+        println("HP:$healthPoints")
+        println()
         return damage
     }
 
-
+    // **Methode um Schaden zu nehmen**
     open fun takeDamage(damage: Int) {
+        // Verringert die Gesundheitspunkte des Helden um den angegebenen Wert
         healthPoints -= damage
-        if (damage == 0) {
-            println()
-        } else
-            if (healthPoints <= 0) {
-                println("Der Feind ist tot")
 
-            } else
-                println("$name hat noch $healthPoints Lebenspunkte")
+        // Gibt eine Nachricht aus, wenn der Held Schaden nimmt
+        if (damage > 0) {
+            println("$name hat $damage Schaden genommen.")
+        }
+
+        // Gibt eine Nachricht aus, wenn der Held stirbt
+        if (healthPoints <= 0) {
+            println("$name ist gestorben.")
+        }
     }
 
+    // **Methode um einen Schrei auszuführen, der die Gesundheitspunkte erhöht**
     open fun shout(buffHealth: Int) {
-        val buff = healthPoints / 100 * 20
-        healthPoints + buff
-    }
-}
+        // Erhöht die Gesundheitspunkte des Helden um den angegebenen Wert
+        healthPoints += buffHealth
 
-
-// Methode nur zum Testen anfanges gebraucht !
-fun printInfoList(hero: MutableList<Hero>) {
-    for (hero in heroes) {
-        println("${hero.name}: Health Points = ${hero.healthPoints}")
+        // Gibt eine Nachricht aus, wenn der Held geheilt wurde
+        if (buffHealth > 0) {
+            println("$name wurde um $buffHealth geheilt.")
+        }
     }
 }
 
 class Soldier(name: String, healthPoints: Int, debuff: Boolean) : Hero(name, healthPoints, debuff) {
-    override fun action(weaponName: String, damage: Int): Int {
-        println("Name:$name")
-        println("HP:$healthPoints")
-        println()
-        println("$name macht seinen Angriff")
-        println()
 
-        return damage
+    override fun action(weaponName: String, damage: Int): Int {
+        // Gibt den Schaden zurück, der durch den Angriff des Soldaten verursacht wird
+        return damage * 2
     }
 }
+
 
 class Scientist(name: String, healthPoints: Int, debuff: Boolean) : Hero(name, healthPoints, debuff) {
 
     override fun action(weaponName: String, damage: Int): Int {
-        println("Name:$name")
-        println("HP:$healthPoints")
-        println()
-        println("$name macht seinen Angriff")
-        println()
+        // Gibt den Schaden zurück, der durch den Angriff des Wissenschaftlers verursacht wird
         return damage
     }
 }
 
 class Jaffa(name: String, healthPoints: Int, debuff: Boolean) : Hero(name, healthPoints, debuff) {
+
     override fun action(weaponName: String, damage: Int): Int {
-        println("Name:$name")
-        println("HP:$healthPoints")
-        println()
-        println("$name macht seinen Angriff")
-        println()
-        return damage
+        // Gibt den Schaden zurück, der durch den Angriff des Jaffas verursacht wird
+        return damage + 10
     }
 }
 
